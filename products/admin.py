723ad3640a9eae72c4d57f8823b06083eb358a9a
NewDAun-jpg <efimovoleg03@mail.ru>
2026-01-09 07:53:56 -0800

    8

--- products/admin.py
+++ products/admin.py
@@ -1,16 +1,9 @@
 from django.contrib import admin
-from .models import  Product
-from .models import Category
-from .models import Cart
-from .models import CartItem
-
-admin.site.register(Cart)
-admin.site.register(CartItem)
+from .models import Category, Product, UniqueUser, Wishlist, Cart, CartItem
 
 admin.site.register(Category)
-
-
 admin.site.register(Product)
-
-
-
+admin.site.register(UniqueUser)
+admin.site.register(Wishlist)
+admin.site.register(Cart)
+admin.site.register(CartItem)
