04ff89817a33b5b126a077786264f2d575051080
NewDAun-jpg <efimovoleg03@mail.ru>
2026-01-11 06:28:07 -0800

    8

--- my_shop/urls.py
+++ my_shop/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from products import views
@@ -16,3 +16,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+#urlpatterns = [
+    #path('products|/',include('products.urls'))
+    #path('my_shop/',include('my_shop.urls'))
