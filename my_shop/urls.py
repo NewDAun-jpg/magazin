688ba7e3ccc0456cd597c9f2958b4f86794bd3b8
NewDAun-jpg <efimovoleg03@mail.ru>
2026-01-19 04:51:48 -0800

    10

--- my_shop/urls.py
+++ my_shop/urls.py
@@ -2,13 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from products import views
 
 urlpatterns = [
     path('', include('core.urls')),
+    path('products/', include('products.urls')),
     path('admin/', admin.site.urls),
-    path('register/', views.register_view, name='register'),
-    path('products/', include('products.urls'))
 ]
 
 if settings.DEBUG:#настройка для полноценной работы изображений для продуктов
