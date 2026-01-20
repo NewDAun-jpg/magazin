439e51de5abc903ae16b3f2a16d78c3cc8d15b5d
NewDAun-jpg <efimovoleg03@mail.ru>
2026-01-20 05:52:20 -0800

    7

--- core/models.py
+++ core/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='images/')
+    phone_number = PhoneNumberField(blank=True, null=False,unique=True)
