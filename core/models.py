466abbe2755406a1813135183221d1743bf97ca0
NewDAun-jpg <efimovoleg03@mail.ru>
2026-01-22 05:47:27 -0800

    13

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.db.models.fields import TextField
 from phonenumber_field.modelfields import PhoneNumberField
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE) 
     avatar = models.ImageField(upload_to='avatar/', null=True, blank=True)
     phone_number = PhoneNumberField(blank=True, null=False,unique=True)
     data_birthday = models.DateField(null=True, blank=True)
