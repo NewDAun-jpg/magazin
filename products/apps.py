fd87490ea6e39ca5b84a35c3acfb4e0e2b7f8768
NewDAun-jpg <efimovoleg03@mail.ru>
2026-02-08 04:59:36 -0800

    19

--- products/apps.py
+++ products/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class ProductsConfig(AppConfig):
     name = 'products'
+
+class UserConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'users'
