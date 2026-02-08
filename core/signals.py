3f72cec646b7089a48be3351755c130fca257d1b
NewDAun-jpg <efimovoleg03@mail.ru>
2026-02-08 06:54:25 -0800

    19

--- core/signals.py
+++ core/signals.py
@@ -6,4 +6,11 @@ from .models import pass #модель пользователя
 User = get_user_model() #берем существующуу модель
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs): #модель отправивший сигнал, конкретный сохраненный обьект,
+def create_user_profile(sender, instance, created, **kwargs):
+    #sender - модель, которая отправила сигнал (User)
+    #instance - конкретный сохраненный объект (конкретный пользователь)
+    #created - True если объект создан, False если обновлен
+    #**kwargs - дополнительные аргументы
+    if created:
+        User.objects.create(username=instance.username, email=instance.email)
+
