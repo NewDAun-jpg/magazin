8b6cde0f3ceac8bb88d367baa2f7b5a7527d3fcf
NewDAun-jpg <efimovoleg03@mail.ru>
2026-02-08 21:43:24 -0800

    20

--- core/signals.py
+++ core/signals.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save #сохраняет все после save()
 from django.dispatch import receiver #привязывает вюьюху к сигналу
-from .models import pass #модель пользователя
+from .models import Profile #модель пользователя
+from django.contrib.auth.models import User
 
 User = get_user_model() #берем существующуу модель
 
@@ -13,4 +14,8 @@ def create_user_profile(sender, instance, created, **kwargs):
     #**kwargs - дополнительные аргументы
     if created:
         User.objects.create(username=instance.username, email=instance.email)
+
+def create_user_profilee(sender, instance, created, **kwargs):
+    if not created:
+        print(f'ользователь обновлеНН {instance.username}')
 
