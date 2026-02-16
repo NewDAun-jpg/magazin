13ccd1940f3e8d1b00d1e6c7bc0111a138c604a3
NewDAun-jpg <efimovoleg03@mail.ru>
2026-02-16 07:12:47 -0800

    23

--- cart/views.py
+++ cart/views.py
@@ -3,13 +3,25 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from products.models import Product
+from .models import Cart
+from .models import CartItem
 
 
 
 @login_required
 def add_cart(request):
     if request.method == 'GET':
-        
+        product_id = request.GET.get('product_id') #берем id из бд
+        product = Product.objects.get(id=product_id) #проверка на то ли взяли
+
+        usercart
+
+        #проверка на нахождение записи
+        cartitem = CartItem.objects.get(user=request.user, product=product)
+
+
+
+
 
 
 
