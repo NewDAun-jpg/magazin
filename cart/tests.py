4a2491324eb939d1af0dd44fe447a90e7148bc27
NewDAun-jpg <efimovoleg03@mail.ru>
2026-02-22 23:27:01 +0800

    25

--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Cart,CartItem
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class CartTestAdd(TestCase):
+    
+
