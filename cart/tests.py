f9fe826852f11029fd4487e2aaa1928d3d3be3d1
NewDAun-jpg <efimovoleg03@mail.ru>
2026-02-23 00:18:52 +0800

    25

--- cart/tests.py
+++ cart/tests.py
@@ -1,10 +1,20 @@
 from django.test import TestCase
 from django.urls import reverse
 from .models import Cart,CartItem
+from products.models import Product,Category
 from django.contrib.auth.models import User
 
 class CartTestAdd(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='papa', email='', password='')
-        self.cart = Cart.objects.create(user=self.user)
+        self.category = Category.objects.create(name='Category')
+        self.product = Product.objects.create(
+            name='Tovar',
+            price=5.30,
+            description='Tovar',
+            category=self.category
+        )
+
+
+
 
