9c1a6250a813aebccfc42c8a29ad7a0da1cf97b7
NewDAun-jpg <efimovoleg03@mail.ru>
2026-02-22 23:53:30 +0800

    25

--- cart/tests.py
+++ cart/tests.py
@@ -4,5 +4,7 @@ from .models import Cart,CartItem
 from django.contrib.auth.models import User
 
 class CartTestAdd(TestCase):
-    
+    def setUp(self):
+        self.user = User.objects.create_user(username='papa', email='', password='')
+        self.cart = Cart.objects.create(user=self.user)
 
