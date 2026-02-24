f09c7f3828d36037b360de3a2a8fd7ff14395705
NewDAun-jpg <efimovoleg03@mail.ru>
2026-02-24 23:23:00 +0800

    25

--- cart/tests.py
+++ cart/tests.py
@@ -1,16 +1,10 @@
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
-from typing import Any
 from products.models import Product, Category
 from .models import CartItem
 
-
 class CartTestAdd(TestCase):
-    def __init__(self, methodName: str = "runTest"):
-        super().__init__(methodName)
-        self.cart = None
-
     def setUp(self):
         self.user = User.objects.create_user(username='papa', email='', password='')
         self.category = Category.objects.create(name='Category')
@@ -22,15 +16,11 @@ class CartTestAdd(TestCase):
         )
 
     def test_add_cart_first_time(self):
-        self.client.force_login(self.user) #регестририуме клиента
-        response = self.client.get(reverse('cart:add_cart'), {'product_id':self.product.id})#наш клиент видит,что в корзину добавил то,что хотел
-        self.assertEqual(response.status_code, 200)#вывод 200 что все работает
-        cart_item = CartItem.objects.get(cart_user=self.cart, product=self.product).first()#связывает пользователя и корзину и так же продуктыи продукты пользователя
-        self.assertEqual(cart_item.quantity, 1)#если все нормально,то количество товаров ==1
-
-
-
-
-
-
-
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('cart:add_cart'), {'product_id': self.product.id})
+        # Проверяем, что запрос выполнен успешно
+        self.assertEqual(response.status_code, 302)  # если вьюха редиректит, или 200,или 302
+        # Получаем элемент корзины и связь пользователя
+        cart_item = CartItem.objects.filter(cart__user=self.user, product=self.product).first()
+        self.assertIsNotNone(cart_item)
+        self.assertEqual(cart_item.quantity, 1)
