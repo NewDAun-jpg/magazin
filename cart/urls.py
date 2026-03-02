ff25908a21e34b2c672c39ad27f69d5a1b0d96ee
NewDAun-jpg <efimovoleg03@mail.ru>
2026-03-02 21:52:20 +0800

    27

--- cart/urls.py
+++ cart/urls.py
@@ -6,6 +6,6 @@ app_name = "cart"
 urlpatterns = [
     path('', views.cart_detail, name='cart_detail'),
     path('add_cart/', views.add_cart, name='add_cart'),
-    path('delete_cart/', views.delete_cart, name='delet_cart'),
+    path('delete_cart/', views.delete_cart, name='delete_cart'),
     path('change_quantity_cart/', views.change_quantity_cart, name='change_quantity_cart'),
 ]
