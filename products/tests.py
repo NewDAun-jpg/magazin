3a72ca0a94b02ee73fa37f8241a2dd3d8d1a8eb9
NewDAun-jpg <efimovoleg03@mail.ru>
2026-03-12 23:28:56 +0800

    33

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+from .models import Wishlist,Product
 
-# Create your tests here.
+class WishlistTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='john', email='', password='')
+        self.wishlist = Wishlist.objects.create(user=self.user)
+        self.product = Product.objects.create(
+            name='Tovar',
+            price=500,
+            description='Tovar',
+        )
+
+    def test_add_wishlist(self):
+        self.client.force_login(self.user)
+        response = self.client.get(reverse('add_wishlist'))
+
+
+
+
