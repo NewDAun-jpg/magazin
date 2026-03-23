d01681c4d98ff613306f031bef49f6b33556ae38
NewDAun-jpg <efimovoleg03@mail.ru>
2026-03-23 23:01:20 +0800

    38

--- products/views.py
+++ products/views.py
@@ -24,7 +24,6 @@ def delivery_detail(request):
 @login_required
 def wishlist(request):
     wishlist_items = Wishlist.objects.filter(user=request.user)
-    print(wishlist_items)
     return render(request, 'products/wishlist_page.html', {'wishlist_items': wishlist_items})
 
 
